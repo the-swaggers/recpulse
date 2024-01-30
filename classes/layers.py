b8a55c0fc8fbd86012cb2bf4bcb09b1816e4e4c8
Maksym Petrenko <maksym.petrenko.a@gmail.com>
2024-01-30 20:40:33 +0000

    make dense layer class

--- classes/layers.py
+++ classes/layers.py
@@ -12,9 +12,11 @@ class Dense(BaseModel):
     """
 
     output_size: StrictInt
+    input_size: StrictInt | None = None
     activation: ACTIVATION_FUNCTIONS = "linear"
     name: StrictStr | None = None
-    weights: list[np.float16 | np.float32 | np.float64] | None = None
+    _bias: float | None = None
+    _weights: list[float] | None = None
 
     @model_validator(mode="after")
     def validator(self) -> None:
@@ -24,8 +26,28 @@ class Dense(BaseModel):
             raise ValidationError("Output size must be greater than 0.")
 
     def initialize_weights(
-        self, input_shape: int, mean: float | None = None, variance: float | None = None
+        self,
+        input_size: int | None = None,
+        mean: float | None = None,
+        variance: float | None = None,
     ) -> None:
         """Weights initializer."""
 
-        return None
+        if self.input_size is None:
+            assert (
+                input_size is not None
+            ), "input_size is not specified neither in the function execution, nor in the class's instance"
+            self.input_size = input_size
+
+        if variance is None:
+            variance = 1
+        elif variance <= 0:
+            raise ValueError("variance must be greater than zero")
+
+        if mean is None:
+            mean = 0
+
+        self._weights = np.random.normal(
+            mean, variance, size=(input_size, self.output_size)
+        )
+        self._bias = np.random.normal(mean, variance)
