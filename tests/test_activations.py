2f5c1b866b315873f18f18b5aadf8a38c0c5a2ce
Maksym Petrenko <maksym.petrenko.a@gmail.com>
2024-02-04 14:26:10 +0000

    make tests more scalable

--- tests/test_activations.py
+++ tests/test_activations.py
@@ -13,14 +13,18 @@ with open("tests/test_data/ndarrays.json") as f:
 
 class TestClassActivations:
     @staticmethod
-    def linear(x):
-        return x
-
-    def test_linear(self):
+    def match_functions(function, activation):
         for array in NDARRAYS.values():
-            function_answer = activations.linear(array)
+            function_answer = activation(array)
             # get a list of all indices
             indices = list(zip(*[axis.flatten() for axis in np.indices(array.shape)]))
 
             for index in indices:
-                assert self.linear(array[index]) == function_answer[index]
+                assert function(array[index]) == function_answer[index]
+
+    @staticmethod
+    def linear(x):
+        return x
+
+    def test_linear(self):
+        self.match_functions(self.linear, activations.linear)
