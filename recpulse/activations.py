6f121bd9940d0d20758ef97ecb5e38829fa96222
Maksym Petrenko <maksym.petrenko.a@gmail.com>
2024-03-01 15:36:06 +0000

    add usage of numba for activations

--- recpulse/activations.py
+++ recpulse/activations.py
@@ -19,20 +19,24 @@ License: MIT
 ================================================================
 """
 import numpy as np
+from numba import njit  # type: ignore
 
 from recpulse.np_dtypes import TENSOR_TYPE
 
 
+@njit
 def linear(x: TENSOR_TYPE) -> TENSOR_TYPE:
     """Linear activation function."""
     return x
 
 
+@njit
 def sigmoid(x: TENSOR_TYPE) -> TENSOR_TYPE:
     """Sigmoid activation function."""
     return 1 / (1 + np.exp(-x))
 
 
+@njit
 def softmax(x: TENSOR_TYPE) -> TENSOR_TYPE:
     """Softmax activation function."""
     x = x - np.max(x)
@@ -40,36 +44,43 @@ def softmax(x: TENSOR_TYPE) -> TENSOR_TYPE:
     return exponentiated / np.sum(exponentiated)
 
 
+@njit
 def relu(x: TENSOR_TYPE) -> TENSOR_TYPE:
     """Relu activation function."""
     return np.where(x >= 0, x, 0)
 
 
+@njit
 def leaky_relu(x: TENSOR_TYPE) -> TENSOR_TYPE:
     """Leaky Relu activation function."""
     return np.where(x >= 0, x, 0.1 * x)
 
 
+@njit
 def parametric_relu(x: TENSOR_TYPE, alpha: float = 0) -> TENSOR_TYPE:
     """Parametric Relu activation function."""
     return np.where(x >= 0, x, alpha * x)
 
 
+@njit
 def tanh(x: TENSOR_TYPE) -> TENSOR_TYPE:
     """Tanh activation function."""
     return np.tanh(x)
 
 
+@njit
 def arctan(x: TENSOR_TYPE) -> TENSOR_TYPE:
     """Arctan activation function."""
     return np.arctan(x)
 
 
+@njit
 def elu(x: TENSOR_TYPE, alpha: float = 1) -> TENSOR_TYPE:
     """Elu activation function."""
     return np.where(x >= 0, x, alpha * (np.exp(-x) - 1))
 
 
+@njit
 def swish(x: TENSOR_TYPE) -> TENSOR_TYPE:
     """Swish activation function."""
     return x / (1 + np.exp(-x))
