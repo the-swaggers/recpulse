a0361fb04151d9884c0509144e31b3b7f4080c6e
maksym-petrenko <maksym.petrenko.a@gmail.com>
2024-11-26 23:07:40 +0000

    add number data classes

---
+++ cuda_utils/dtypes.py
@@ -0,0 +1,12 @@
+class bool: pass
+class float8: pass
+class float16: pass
+class float32: pass
+class float64: pass
+
+bool_ = bool
+Float8, fp8 = float8, float8
+Float16, fp16 = float16, float16
+Float32, fp32 = float32, float32
+Float64, fp64 = float64, float64
+
