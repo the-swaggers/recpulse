c7130b4071ca4b2e25a0eef1ae83c83b7857c153
maksym-petrenko <maksym.petrenko.a@gmail.com>
2025-05-14 23:59:34 +0000

    add datatypes

--- cuda_utils/tensor.h
+++ cuda_utils/tensor.h
@@ -4,14 +4,26 @@
 #ifndef TENSOR_H
 #define TENSOR_H
 
+typedef enum {
+    float8,
+    float16,
+    float32,
+    float64,
+} DType;
+
+typedef enum {
+    DEVICE_CPU,
+    DEVICE_CUDA,
+} DeviceType;
 
 struct Tensor{
     size_t size;
-    ndim uint;
-    shape *int;
-    vals *void;
-    <idk what here> dtype;
-    device *char;
+    uint ndim;
+    *int shape;
+    *void vals;
+    DType dtype;
+    DeviceType device_type;
+    uint device_id;
     requires_grad bool;
     owns_data bool;
 }
