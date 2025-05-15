45bba8501fb0745fd3bc44b0b5ffa8d268c89ded
maksym-petrenko <maksym.petrenko.a@gmail.com>
2025-05-15 00:52:28 +0000

    init tensor.c file

---
+++ cuda_utils/tensor.c
@@ -0,0 +1,42 @@
+#include "tensor.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+Tensor* tensor_new(uint32_t ndim, int* shape, void* vals, DType dtype, DeviceType device_type, uint32_t device_id, bool requires_grad, bool owns_data){
+    assert(!(dtype == DTYPE_FLOAT8 & device_type == DEVICE_CPU) && "CPU does not support float8 operations!")
+
+    int nvals = 1
+    for (int i = 0; i < ndim; i++){
+        nvals = nvals * shape[i]
+    }
+    size_t size = get_size(nvals, dtype);
+
+    Tensor* tensor = (Tensor*)malloc(sizeof(Tensor));
+    tensor->size = size;
+    tensor->ndim = ndim;
+    tensor->shape = shape;
+    tensor->dtype = dtype;
+    tensor->device_type = device_type;
+    tensor->device_id = device_id;
+    tensor->requires_grad = requires_grad;
+    tensor->owns_data = owns_data;
+    
+    return tensor;
+}
+
+
+size_t get_size(int number_of_parameters, DType dtype){
+    switch (dtype) {
+        case DTYPE_FLOAT8:
+            return number_of_parameters;
+        case DTYPE_FLOAT16:
+            return number_of_parameters * 2;
+        case DTYPE_FLOAT32:
+            return number_of_parameters * 4;
+        case DTYPE_FLOAT64:
+            return number_of_parameters * 8;
+    }
+}
+
