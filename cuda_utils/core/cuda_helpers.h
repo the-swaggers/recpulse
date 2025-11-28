3ffb912a1d5d9654cf640b804c6a7141510874fa
maksym-petrenko <maksym.petrenko.a@gmail.com>
2025-11-28 02:52:27 +0100

    add headers

---
+++ cuda_utils/core/cuda_helpers.h
@@ -0,0 +1,14 @@
+#ifndef CUDA_HELPERS_H
+#define CUDA_HELPERS_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <cuda_runtime.h>
+
+bool check_cuda_call(cudaError_t err, const char* operation);
+bool check_cuda_kernel();
+bool check_shape_valid(int ndim, int* shape);
+bool safe_multiply_size(size_t a, size_t b, size_t* result);
+bool calculate_total_elements(int ndim, int* shape, size_t* total);
+
+#endif
