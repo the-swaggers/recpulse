c7f8f1087cdfcf0b757b8791bef703383c25be58
maksym-petrenko <maksym.petrenko.a@gmail.com>
2026-04-12 05:02:14 +0200

    add data loader

--- cuda_utils/recpulse/data.py
+++ cuda_utils/recpulse/data.py
@@ -65,6 +65,57 @@ def _load_idx_labels(path):
     return list(data)
 
 
+def load_cifar10(data_dir='data/cifar10', download=True):
+    import numpy as np
+    import pickle
+    import tarfile
+
+    batch_dir = os.path.join(data_dir, 'cifar-10-batches-py')
+
+    if download and HAS_URLLIB and not os.path.exists(batch_dir):
+        os.makedirs(data_dir, exist_ok=True)
+        tar_path = os.path.join(data_dir, 'cifar-10-python.tar.gz')
+        if not os.path.exists(tar_path):
+            print('Downloading CIFAR-10...')
+            urllib.request.urlretrieve('https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz', tar_path)
+        print('Extracting CIFAR-10...')
+        with tarfile.open(tar_path) as tar:
+            tar.extractall(data_dir)
+
+    def load_batch(path):
+        with open(path, 'rb') as f:
+            d = pickle.load(f, encoding='bytes')
+        return d[b'data'], d[b'labels']
+
+    train_x, train_y = [], []
+    for i in range(1, 6):
+        x, y = load_batch(os.path.join(batch_dir, f'data_batch_{i}'))
+        train_x.append(x)
+        train_y.extend(y)
+
+    train_x = np.concatenate(train_x).reshape(-1, 3, 32, 32).astype(np.float32) / 255.0
+    test_x, test_y = load_batch(os.path.join(batch_dir, 'test_batch'))
+    test_x = np.array(test_x).reshape(-1, 3, 32, 32).astype(np.float32) / 255.0
+
+    train_images = rp.from_numpy(train_x)
+    test_images = rp.from_numpy(test_x)
+
+    return train_images, train_y, test_images, test_y
+
+
+def get_batch_4d(images, labels, batch_idx, batch_size):
+    start = batch_idx * batch_size
+    end = min(start + batch_size, images.shape[0])
+    if start >= images.shape[0]:
+        return None, None
+
+    batch_images = images.slice([start, 0, 0, 0],
+                                [end, images.shape[1], images.shape[2], images.shape[3]],
+                                [1, 1, 1, 1])
+    batch_labels = labels[start:end]
+    return batch_images, batch_labels
+
+
 def get_batch(images, labels, batch_idx, batch_size):
     start = batch_idx * batch_size
     end = min(start + batch_size, images.shape[0])
